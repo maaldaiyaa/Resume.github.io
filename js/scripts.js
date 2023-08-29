c9d3029d13db1f7f7f5afbbf02b3830f4efbacbd
maalasmri <maaldaiyaa@gmail.com>
2023-08-29 10:43:16 +0300

    Added

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const buttons = document.querySelectorAll(".buttonnn");
+    buttons.forEach(button => {
+      button.addEventListener("click", function() {
+        const link = button.getAttribute("data-link");
+        if (link) {
+          window.open(link, "_blank"); 
+        }
+      });
+    });
+  });
+
+
+
+  
